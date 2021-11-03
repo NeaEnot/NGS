647ffa8621b3411a43d738f4081e0e5ef32ec017
NeaEnot <oleg.kozloss@yandex.ru>
2021-11-03 21:55:39 +0400

    Добавьте файлы проекта.

---
+++ Physics/Body.cs
@@ -0,0 +1,29 @@
+﻿namespace Physics
+{
+    /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/Body/*'/>
+    public class Body
+    {
+        /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/Id/*'/>
+        public string Id { get; set; }
+
+        /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/X/*'/>
+        public ulong X { get; set; }
+        /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/Y/*'/>
+        public ulong Y { get; set; }
+        /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/D/*'/>
+        public byte D { get; set; }
+
+        /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/Mass/*'/>
+        public uint Mass { get; set; }
+        /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/Velocity/*'/>
+        public Vector Velocity { get; set; }
+
+        /// <include file='Documentation.xml' path='documentation/members[@name="Body"]/ColorHex/*'/>
+        public string ColorHex { get; set; }
+
+        public Body()
+        {
+            Velocity = new Vector { Vx = 0, Vy = 0 };
+        }
+    }
+}
---
+++ Physics/Vector.cs
@@ -0,0 +1,11 @@
+﻿namespace Physics
+{
+    /// <include file='Documentation.xml' path='documentation/members[@name="Vector"]/Vector/*'/>
+    public class Vector
+    {
+        /// <include file='Documentation.xml' path='documentation/members[@name="Vector"]/Vx/*'/>
+        public double Vx { get; set; }
+        /// <include file='Documentation.xml' path='documentation/members[@name="Vector"]/Vy/*'/>
+        public double Vy { get; set; }
+    }
+}
