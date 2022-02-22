8e27a94bac6ef948877a5d080ff8e0d020960430
NeaEnot <oleg.kozloss@yandex.ru>
2022-02-22 22:30:34 +0400

    Центрирование на центре масс

---
+++ Drawing/Centers/MassCenter.cs
@@ -0,0 +1,25 @@
+﻿using Physics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Drawing.Centers
+{
+    public class MassCenter : ICenter
+    {
+        private List<Body> bodies;
+
+        public Vector Center =>
+            new Vector
+            {
+                Vx = bodies.Sum(req => req.X * req.Mass) / bodies.Sum(req => req.X),
+                Vy = bodies.Sum(req => req.Y * req.Mass) / bodies.Sum(req => req.Y)
+            };
+
+        public bool IsScaled => true;
+
+        public MassCenter(List<Body> bodies)
+        {
+            this.bodies = bodies.ToList();
+        }
+    }
+}
